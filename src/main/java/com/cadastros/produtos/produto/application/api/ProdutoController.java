c01356a402e7b50930e1f089f57a2d690b75d3df
= <gabriel.cirilo.rosa@gmail.com>
2024-04-09 20:42:35 -0300

    Adicao de dependencias para validacao

--- src/main/java/com/cadastros/produtos/produto/application/api/ProdutoController.java
+++ src/main/java/com/cadastros/produtos/produto/application/api/ProdutoController.java
@@ -1,6 +1,7 @@
 package com.cadastros.produtos.produto.application.api;
 
 import com.cadastros.produtos.produto.application.service.ProdutoService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,7 @@ public class ProdutoController implements ProdutoAPI{
     private final ProdutoService produtoService;
 
     @Override
-    public ProdutoResponse cadastraProduto(ProdutoRequest produtoRequest) {
+    public ProdutoResponse cadastraProduto(@Valid ProdutoRequest produtoRequest) {
         log.info("[start] ProdutoController - cadastraProduto");
         ProdutoResponse produtoCriado = produtoService.cadastraProduto(produtoRequest);
         log.info("[finish] ProdutoController - cadastraProduto");
